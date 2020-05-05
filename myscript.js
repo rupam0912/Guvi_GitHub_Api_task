d2cd51651e81ca6c90a6cb31afa12ea9a4405ee0
Rupam <rupam@localhost.localdomain>
2020-05-05 14:51:15 -0400

    followers

--- myscript.js
+++ myscript.js
@@ -20,22 +20,54 @@ function fetchUserData(){
             
             // Insert a row in the table at the last row
             var newRow   = tableRef.insertRow(1);
+
+            var followers = fetch(data.followers_url)
+            .then((response)=> {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then((data)=> {
+                var followers = [];
+                data.forEach((obj)=>{
+                    followers.push(obj.login);
+                });
+                return new Promise((resolve,reject)=> resolve(followers));
+            });
+            
         
             // Insert a cell in the row at index 0
             var newCell1  = newRow.insertCell(0);
             var newCell2  = newRow.insertCell(1);
             var newCell3  = newRow.insertCell(2);
             var newcell4  = newRow.insertCell(3);
+            var newcell5  = newRow.insertCell(4);
         
             // Append a text node to the cell
             newCell1.appendChild(img);
-            newCell2.innerHTML = data.name;
+            newCell2.innerHTML = `<a href=${data.html_url} target="_blank"> ${data.name} </a>`;
             newCell3.innerHTML = data.public_repos;
-            newcell4.innerHTML = `<a href=${data.html_url} target="_blank"> Click Here </a>`;
+            followers.then((val)=>{
+                
+                var flwList = "";
+                val.forEach((val1)=>{
+                    flwList += `<a href=https://github.com/${val1} target="_blank" style="color:red">${val1}</a> </br>`;
+                }); 
+                newcell4.innerHTML = flwList;
+            });
+            if(data.location === null){
+                newcell5.innerHTML = `<p>Location not available</p>`;
+            } else {
+                newcell5.innerHTML = data.location;
+            }
+            
              
             if(noOfRows>2){
                tableRef.deleteRow(3);
             }
+
+
 
         }
 
